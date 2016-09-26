24de73c73f712e3a0680092b63791fc03d688be2
Ali Student 8 <Student 8@10.8.33.160>
2016-09-26 12:32:25 -0400

    9/26

--- Ali Intro/src/chatBot/School.java
+++ Ali Intro/src/chatBot/School.java
@@ -4,11 +4,16 @@ import datatype8.AliBot;
 
 public class School implements Topic {
 	private boolean inSchoolLoop;
-	public String response;
+	public String schoolResponse;
+	public School(){
+		
+	}
 	public void talk() {
 		inSchoolLoop = true;
 		while(inSchoolLoop){
-			AliBot.print("Tell me about school");
+			AliBot.print("Tell me about school.");
+			schoolResponse = AliBot.getInput();
+			i
 			school
 		}
 	}
