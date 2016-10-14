a80a169bbe0e9262db61ce9d9dc2bd92c214efa3
Ali Student 8 <Student 8@10.8.33.160>
2016-10-14 12:33:50 -0400

    10/14/16

--- Ali Intro/src/array/ArraysMain.java
+++ Ali Intro/src/array/ArraysMain.java
@@ -3,28 +3,63 @@ package array;
 public class ArraysMain {
 
 	public static void main(String[] args) {
-		//This is how you time how quickly a computer processes
+
 		long startTime = System.currentTimeMillis();
+		SampleElement sample = new SampleElement(10);
+		sample.increase();
+		System.out.println("The sample element has" + "a number equal to"
+		+sample.getNumber());
+		long endTime = System.currentTimeMillis();
+		System.out.println("Completed method in" +(endTime-startTime)+"milliseconds");
+	}
+	private static void passByValueDemonstration(){
+
+		//This is how you time how quickly a computer processes
+		//arrays get changed, variables don't
+
 		String[] someStrings = new String[100];
 		populateArray(someStrings);
 		changeString(someStrings[99]);
 		printArray(someStrings);
 		arrayIntroMethod();
-		long endTime = System.currentTimeMillis();
-		System.out.println("Completed method in" +(endTime-startTime)+"milliseconds");
+
+		int ten = 10;
+		increase(ten);
+		System.out.println("Ten, increase is"+ten);
+
+		System.out.println("Before "+someStrings[99]);
+		changeString(someStrings[99]);
+		System.out.println("After \"changeString\""+someStrings[99]);
+		printArray(someStrings);
+		changeArrayElement(someStrings,99);
+
+
+	}
+	private static void changeArrayElement(String[] someStrings, int i) {
+		someStrings[i] = "new item "+(i+1);
+	}
+	private static void increase(int x) {
+		x=x+1;
+
 	}
 	private static void changeString(String s){
 		s= "This string has been changed.";
 	}
-	
+
 	private static void printArray(String[] b){
-		
+		for(String a: b){
+			System.out.println(a);
+		}
 	}
-	
+
 	private static void populateArray(String[] a){
-		
+		//this loop instantiating the strings
+		for(int index =0; index < a.length ; index++)
+		{
+			a[index] = "value" + (index+1);
+		}
 	}
-	
+
 	public static void arrayIntroMethod(){
 		// Construct 3 integer arrays
 		//these two methods do the same thing
