a7585f33ab7c530e55d681895c2a3477b0f3656e
Ali Student 8 <Student 8@10.8.33.160>
2016-10-17 13:16:00 -0400

    10/17/16

---
+++ Ali Intro/src/array/ArrayTesting.java
@@ -0,0 +1,19 @@
+package array;
+
+public class ArrayTesting {
+
+	public static void main(String[] args) {
+		int[] number = new int [50];
+		createArray();
+
+	}
+	private static void createArray(){
+		
+	}
+	private static void oneToFifty(){
+		for(int index =0; index < a ; index++)
+		{
+			a[index] = "value" + (index+1);
+		}
+	}
+}
