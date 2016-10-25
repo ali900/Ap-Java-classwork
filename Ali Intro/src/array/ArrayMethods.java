598c0eadab818e0cd4e1359a3dbfbe5ab28bd53a
Ali Student 8 <Student 8@10.8.33.160>
2016-10-25 13:15:11 -0400

    new 10/25

--- Ali Intro/src/array/ArrayMethods.java
+++ Ali Intro/src/array/ArrayMethods.java
@@ -41,4 +41,38 @@ public class ArrayMethods {
 	private static void print(int[] arr){
 		//
 	}
+	public static int countUnderBound(double[] arr, double d){
+		int numberElements = 0;
+		for( int i = 0; i < arr.length; i++){
+			if(arr[i] <d){
+				ctr++;
+			}
+		}
+		return ctr;
+	}
+	public static boolean contains(int[] arr, int[] subArray){
+		for(int i= 0; i < arr.length; i++){
+			if(arr[i] == subArray[0]){
+				//check the rest of the elements
+				int j = 0;
+				while(j < subArray.length){
+					if(subArray[j] == arr[i+j] && j == subArray.length-1){
+						System.out.println("Found a match at index "+i);
+						return true;
+					}else if(subArray[j] != arr[i+j]){
+						System.out.println("No match at "+i);
+						break;
+					}
+					j++;
+				}
+			}
+			return false;
+		}
+	}
+	public static int[] getSubArray(int [] arr, int startIndex, int endIndex){
+		int [] subArray = new int[endIndex - startIndex +1];
+		for(int i = 0; i<subArray.length; i++){
+			subArray[i] = arr[startIndex +i];
+		}
+	}
 }
