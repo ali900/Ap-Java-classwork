db5e897e8b2b855df909c2969f0312d4a4a3246a
Student 6 2.0 <Student 6 2.0@10.8.33.160>
2016-12-09 12:32:55 -0500

    learning

---
+++ Ali Intro/src/introUnit/Calculator.java
@@ -0,0 +1,31 @@
+package introUnit;
+import java.util.Scanner;
+
+class Calculator {
+
+	public static void main(String[] args) {
+		Scanner bucky= new Scanner(System.in);
+		//when u have many variables that are the same type u can do the following:
+		double fnum, snum, answer;
+		System.out.println("Enter your first num: ");
+		// a way to enter number and store in fnum
+		// each data type in java needs its own method so it knows how to save it
+		// .nextLine was for string and .nextDouble can be for numbers
+		fnum= bucky.nextDouble();
+		System.out.println("Enter your second num: ");
+		snum= bucky.nextDouble();
+		answer= fnum + snum;
+		System.out.println(answer);
+	}
+
+}
+
+class MathOperators{
+	public static void main(String[] args){
+		Scanner bucky= new Scanner(System.in);
+		int girls, boys, people;
+		girls = 6;
+		boys = 3;
+		people= boys + girls;
+	}
+}
---
+++ Ali Intro/src/introUnit/Learning.java
@@ -0,0 +1,13 @@
+package introUnit;
+import java.util.Scanner;
+//scanner is useful for user input
+class Learning {
+
+	public static void main(String[] args) {
+		Scanner bucky= new Scanner(System.in);
+		System.out.println(bucky.nextLine());
+		//next line waits until you get input from line 7
+
+	}
+
+}
