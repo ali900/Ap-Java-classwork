5d7dc10aa3d381dda8d5eaecf7829b0ac38a2e27
Student 6 2.0 <Student 6 2.0@10.8.33.160>
2016-12-13 12:31:13 -0500

    12/13/16 learning

---
+++ Ali Intro/src/introUnit/LearningClassesPart2.java
@@ -0,0 +1,13 @@
+package introUnit;
+
+public class LearningClassesPart2 {
+
+	public static void main(String[] args) {
+		//we have this object because we want to use something from the tuna class
+		LearningMultipleClasses LearningMultipleClassesObject = new LearningMultipleClasses();
+		//how to use a specific message by using an object of that class
+		LearningMultipleClassesObject.simpleMessage();
+
+	}
+
+}
---
+++ Ali Intro/src/introUnit/LearningMultipleClasses.java
@@ -0,0 +1,9 @@
+package introUnit;
+
+public class LearningMultipleClasses {
+	public static void simpleMessage(){
+		System.out.println("THis is another class");
+		
+	}
+
+}
